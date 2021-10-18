2df5f1c99a8033179aadd47c4bad1070fcb6aa24
Karan2021 <90594223+Karan2021@users.noreply.github.com>
2021-10-18 12:08:36 +0530

    Add files via upload

---
+++ 5th Assignment - ASP.NET 17-10-21/EventManagementApp/EventBusiness/MyEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventDataClass;
+using System.Data;
+
+namespace EventBusiness
+{
+    public class MyEvent
+    {
+        MyDataOperation MyDataOperationObj = new MyDataOperation();
+
+        public void InsertEvent(Login logindata)
+        {
+            MyDataOperationObj.InsertEvent(logindata);
+        }
+        public void InsertVenue(VenueTable venuedata)
+        {
+            MyDataOperationObj.InsertVenue(venuedata);
+        }
+
+        public void BookEvent(BookingTable bookingTable)
+        {
+            MyDataOperationObj.BookEvent(bookingTable);
+        }
+
+        
+    }
+}
---
+++ 5th Assignment - ASP.NET 17-10-21/EventManagementApp/EventDataClass/MyDataOperation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+
+namespace EventDataClass
+{
+    public class MyDataOperation
+    {
+        dbEventEntities MydbEventEntities = new dbEventEntities();
+
+        public void InsertEvent(Login LoginObj)
+        {
+            MydbEventEntities.Logins.Add(LoginObj);
+            MydbEventEntities.SaveChanges();
+        }
+
+        public void InsertVenue(VenueTable VenueObj)
+        {
+            MydbEventEntities.VenueTables.Add(VenueObj);
+            MydbEventEntities.SaveChanges();
+        }
+        public void BookEvent(BookingTable bookingTable)
+        {
+            MydbEventEntities.BookingTables.Add(bookingTable);
+            MydbEventEntities.SaveChanges();
+        }
+       
+
+       
+    }
+}
